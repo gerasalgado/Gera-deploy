a593d5a74905198efda605d30dc81ed42dd0986b
gerasalgado <geradiler@hotmail.com>
2022-11-07 18:06:02 -0600

    fix JSON

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Gerardo Salgado</p>
+        <p>Gera Salgado</p>
         <a
           className="App-link"
           href="https://reactjs.org"
